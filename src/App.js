52a88ed979e27e5bce6637dd08fb901ed36a8b7b
Utsav <utsavkumar5462@gmail.com>
2023-02-28 12:41:32 +0530

    Displaying title using javascript object

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import logo from "./logo.svg";
 import "./App.css";
 
 const title = "React";
+const welcome = {
+  greeting: "Hey",
+  title: "Love You 3000",
+};
 
 function App() {
   return (
     <div>
       <h1>Hello World :)</h1>
       <h1>Hello {title} ;)</h1>
+      <h1>
+        {/* Remember, everything in curly braces in JSX can be used for JavaScript expressions */}
+        {welcome.greeting}!! {welcome.title}
+      </h1>
     </div>
   );
 }
